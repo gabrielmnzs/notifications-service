eb8f0b5767645f7d1ac90de4c80649f1c446d31b
Gabriel Menezes <gabrielmnzs7@gmail.com>
2024-02-10 17:42:26 -0300

    chore: move files to new folder structure

--- src/infra/app.controller.ts
+++ src/infra/app.controller.ts
@@ -2,8 +2,8 @@ import { randomUUID } from 'node:crypto';
 
 import { Body, Controller, Get, Post } from '@nestjs/common';
 
-import { PrismaService } from './prisma.service';
-import { CreateNotificationBody } from './create-notification-body';
+import { PrismaService } from './infra/prisma.service';
+import { CreateNotificationBody } from './infra/create-notification-body';
 
 @Controller('notifications')
 export class AppController {
