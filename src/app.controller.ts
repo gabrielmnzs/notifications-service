7d62785245ba057f777c2c6c3a7432785a76a7f6
Gabriel Menezes <gabrielmnzs7@gmail.com>
2024-02-10 16:36:53 -0300

    :tada: initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,30 @@
+import { randomUUID } from 'node:crypto';
+
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+import { PrismaService } from './prisma.service';
+import { CreateNotificationBody } from './create-notification-body';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  @Get()
+  list() {
+    return this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
